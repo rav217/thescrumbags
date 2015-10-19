714be4ad817f8329949e5b070f6561fab4bac913
Chris Byam <crb217@lehigh.edu>
2015-10-19 02:44:38 -0400

    DBHandler class

---
+++ Classes/DBHandler.java
@@ -0,0 +1,13 @@
+/*
+class that will interact with the DB
+ */
+package thescrumbags.Classes;
+
+/**
+ *
+ * @author Chris
+ */
+public class DBHandler {
+    //will work on this tomorrow. MySQL DB is up and running
+    
+}
