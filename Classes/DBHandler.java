4c4c5e4464d90c1477de00372baaa5305379c285
Chris Byam <crb217@lehigh.edu>
2015-10-19 21:52:27 -0400

    Progress on DBhandler

--- Classes/DBHandler.java
+++ Classes/DBHandler.java
@@ -4,6 +4,8 @@ class that will interact with the DB
 package thescrumbags.Classes;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
@@ -11,11 +13,12 @@ import java.sql.SQLException;
  * @author Chris
  */
 public class DBHandler {
+    
     public static void main(String[] args){
         
         System.out.println("Loading JDBC driver...");
         try{
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("java.sql.Driver");
         } catch (ClassNotFoundException ex){
             //error establishing connection
             System.out.println("Error loading JDBC driver.");
@@ -27,12 +30,12 @@ public class DBHandler {
         System.out.println("Connecting to database...");
         Connection conn = null;
         try{
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/ScrumbagsDB", "ChrisByam", "tolland17");
+            conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/?user=ChrisByam", "ChrisByam", "tolland17");
         } catch (SQLException ex){
             System.out.println("Error connecting to database.");
             ex.printStackTrace();
             return;
         }
-        System.out.println("Successfully connected to database");
+        System.out.println("Successfully connected to database.");
     }
 }   
