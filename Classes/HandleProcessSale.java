8ad8014416669bcef20734d63eafb7c4d303687a
TJ Marrapodi <tjm217@lehigh.edu>
2015-10-26 19:33:55 -0400

    Pushing new main class and process sale handler

--- Classes/HandleProcessSale.java
+++ Classes/HandleProcessSale.java
@@ -1,5 +1,6 @@
-import java.util.Scanner;
+package thescrumbags.Classes;
 
+import java.util.Scanner;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,6 +11,7 @@ import java.util.Scanner;
  *
  * @author TJ
  */
+
 //handles the Process Sale use case
 public class HandleProcessSale {
  
