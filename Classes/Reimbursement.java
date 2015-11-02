249c7e8fe3b1333f105e8fb2b65f3cc254dcb117
Ben Candell <bsc218@lehigh.edu>
2015-11-02 03:18:42 -0500

    Created Reimbursement class

---
+++ Classes/Reimbursement.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package thescrumbags.Classes;
+
+import java.math.BigDecimal;
+
+/**
+ *
+ * @author benscandell
+ */
+public class Reimbursement {
+    
+    private Money amount;
+    
+    public Reimbursement() {
+        this.amount=amount.multiply(new BigDecimal(-1));
+    }
+    public Money getAmount() { return amount; } 
+    
+}
