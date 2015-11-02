034323e627364c0e35791482daa8e349e756418c
Ben Candell <bsc218@lehigh.edu>
2015-11-02 00:16:01 -0500

    Added equals() method and made fields final

--- Classes/ProductDescription.java
+++ Classes/ProductDescription.java
@@ -1,29 +1,36 @@
 package thescrumbags.Classes;
 
 /**
- * Ben Candell
- * bsc218@lehigh.edu
- * CSE 216
- * ProductDescription class
+ * Ben Candell bsc218@lehigh.edu CSE 216 ProductDescription class
  */
-
 /**
- * A class to describe a product 
+ * A class to describe a product
  */
 public class ProductDescription {
-	private Money price;
-	private int id;
-	private String description;
-	
-	public ProductDescription(int id, Money price, String description) {
-		this.id=id;
-		this.price=price;
-		this.description=description;
-	}
-	
-	public int getItemID() { return id; }
-	
-	public Money getPrice() { return price; }
-	
-	public String getDescription() { return description; }
+
+    private final Money price;
+    private final int id;
+    private final String description;
+
+    public ProductDescription(int id, Money price, String description) {
+        this.id = id;
+        this.price = price;
+        this.description = description;
+    }
+
+    public int getItemID() {
+        return id;
+    }
+
+    public Money getPrice() {
+        return price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    
+    public boolean equals(ProductDescription pd) { 
+        return pd.getItemID()==id;
+    }
 }
