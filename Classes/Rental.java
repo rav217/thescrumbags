0d820184e2b421a5d3207cb6f0ea2669ccdd0945
Chris Byam <crb217@lehigh.edu>
2015-11-02 23:17:16 -0500

    remix to init PC method

--- Classes/Rental.java
+++ Classes/Rental.java
@@ -56,6 +56,11 @@ public class Rental extends Transaction {
     
     @Override
     public void updateInventory() {
+        DBHandler db = DBHandler.getInstance();
+        db.openConnection("sql595207", "nT1*rF4!");
+        db.addTransaction("R", lineItems);
+        db.updateInventory("rentalproducts", lineItems);
+        db.closeConnection();
         
     }
 }
