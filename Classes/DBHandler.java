137e3db90e69902a57d1f73901cf340e5f9cfd1a
TJ Marrapodi <tjm217@lehigh.edu>
2015-11-03 20:40:02 -0500

    throwing an exception if transaction id entered is invalid

--- Classes/DBHandler.java
+++ Classes/DBHandler.java
@@ -62,97 +62,6 @@ public class DBHandler {
         return false;
     }
 
-/*not essential to POS_system functionality at this time...
-    
-    //fetches item from db specified by the id. returns false on success, true on failure
-    public boolean fetchItem(int id) {
-        System.out.println("Searching...");
-        String descr = null;
-        double price = 0;
-        String query = "select * from saleproducts where id = " + id;
-        try {
-            stmt = conn.createStatement();
-            rs = stmt.executeQuery(query);
-            while (rs.next()) {
-                descr = rs.getString("descr");
-                price = rs.getDouble("price");
-            }
-        } catch (SQLException ex) {
-            System.out.println("Error connecting to system.");
-            System.out.println("");
-            return true;
-        }
-        if (descr == null || price == 0) {
-            System.out.println("Item ID " + id + " not found.");
-            System.out.println("");
-            return true;
-        } else {
-            System.out.println("Item with ID " + id + " is " + descr + " and costs $" + price + ".");
-            System.out.println("");
-        }
-        return false;
-    }
-
-    public boolean addItem(int id, double price, String descr) {
-        String query = "insert into saleproducts values (" + id + ", " + price + ", '" + descr + "')";
-        System.out.println(query);
-        System.out.println("");
-        try {
-            stmt = conn.createStatement();
-            stmt.executeUpdate(query);
-        } catch (SQLException ex) {
-            System.out.println("Error adding item to system.");
-            System.out.println("");
-            return true;
-        }
-        System.out.println("Item successfully added to system.");
-        System.out.println("");
-        return false;
-    }
-
-    public boolean removeItem(int id) {
-        String query = "delete from saleproducts where id = " + id;
-        System.out.println("");
-        try {
-            stmt = conn.createStatement();
-            stmt.executeUpdate(query); //is the error here?
-        } catch (SQLException ex) {
-            System.out.println("Error removing item from system.");
-            System.out.println("");
-            return true;
-        }
-        System.out.println("Item successfully removed from system.");
-        System.out.println("");
-        return false;
-    }
-
-    public void displayItems() {
-        String descr = null;
-        double price = 0;
-        int id = 0;
-        String query = "select * from saleproducts";
-        System.out.println("ItemID\tDescription\t\t\tPrice");
-        System.out.println("-----------------------------------------------");
-        try {
-            stmt = conn.createStatement();
-            rs = stmt.executeQuery(query);
-            while (rs.next()) {
-                id = rs.getInt("id");
-                price = rs.getDouble("price");
-                descr = rs.getString("descr");
-                if (descr.length() > 16) {
-                    System.out.println(id + "\t" + descr + "\t" + price);
-                } else {
-                    System.out.println(id + "\t" + descr + "\t\t\t" + price);
-                }
-            }
-        } catch (SQLException ex) {
-            System.out.println("Error viewing items in the system.");
-            System.out.println("");
-        }
-        System.out.println("");
-    }
-*/
     public void closeConnection() {
         try {
             conn.close();
@@ -395,6 +304,18 @@ public class DBHandler {
         try{
             stmt = conn.createStatement();
             rs = stmt.executeQuery(query);
+            
+            int size = 0;
+            if(rs != null)
+            {
+                rs.beforeFirst();
+                rs.last();
+                size = rs.getRow();
+            }
+            if(size == 0)
+            {
+                throw new NullPointerException();
+            }
             while(rs.next()){
                 //info required for ProductDescription object
                 String descr = rs.getString("descr");
