4d5d8255c3824163febcd34fff652a546bb23ff9
Ben Candell <bsc218@lehigh.edu>
2015-11-14 17:38:48 -0500

    Created SaleReceipt class

---
+++ Classes/SaleReceipt.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package thescrumbags.Classes;
+
+/**
+ *
+ * @author benscandell
+ */
+public class SaleReceipt extends Receipt {
+    public SaleReceipt() {}
+    
+    @Override
+    public void makeReceipt(Transaction t) throws ClassCastException {
+        if(t instanceof Sale) { 
+            Sale s=(Sale)t;
+            
+        } else {
+            throw new ClassCastException();
+        }
+    }
+}
