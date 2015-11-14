597d598420dc7707459a921bf8bd18b71bbd03fb
Ben Candell <bsc218@lehigh.edu>
2015-11-14 18:31:49 -0500

    Added email properties to Receipt

--- Classes/Receipt.java
+++ Classes/Receipt.java
@@ -5,12 +5,51 @@
  */
 package thescrumbags.Classes;
 
+import java.util.*;
+import javax.mail.*;
+import javax.mail.internet.*;
+
 /**
  *
- * @author benscandell
+ * @author The Scrumbags
  */
 public abstract class Receipt {
-    public Receipt() {}
+    
+    private static final String userName="scrumbagspos";
+    private static final String password="scrumbags";
+    private final String recipient;
+    
+    public Receipt(String recipient) {
+        this.recipient=recipient;
+    }
+    
     public abstract void makeReceipt(Transaction t);
-    public abstract void printReceipt();
+    
+    public void sendReceipt() {
+        Properties props=System.getProperties();
+        String host="smtp.gmail.com";
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.user", userName);
+        props.put("mail.smtp.password", password);
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+        
+        Session session = Session.getDefaultInstance(props);
+        MimeMessage message = new MimeMessage(session);
+        try {
+            message.setSubject("Your scrumbags receipt");
+            message.setText("");
+            Transport transport = session.getTransport("smtp");
+            transport.connect(host, userName, password);
+            transport.sendMessage(message, message.getAllRecipients());
+            transport.close();
+        }
+        catch (AddressException ae) {
+            
+        }
+        catch (MessagingException mex) {
+            
+        }
+    }
 }
