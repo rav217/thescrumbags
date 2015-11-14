1d5c013658cd0e127d1d47ce764be546db33fe1b
Ben Candell <bsc218@lehigh.edu>
2015-11-14 17:38:29 -0500

    Created Receipt class

---
+++ Classes/Receipt.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package thescrumbags.Classes;
+
+/**
+ *
+ * @author benscandell
+ */
+public abstract class Receipt {
+    public Receipt() {}
+    public abstract void makeReceipt(Transaction t);
+}
