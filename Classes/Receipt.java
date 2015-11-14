0a8f89f8ce975639247adc2ef92d72060be899dd
Ben Candell <bsc218@lehigh.edu>
2015-11-14 18:43:18 -0500

    Added email functionality to Receipt. Currently working

--- Classes/Receipt.java
+++ Classes/Receipt.java
@@ -16,16 +16,20 @@ import javax.mail.internet.*;
 public abstract class Receipt {
     
     private static final String userName="scrumbagspos";
-    private static final String password="scrumbags";
+    private static final String password="scrumbags3";
     private final String recipient;
     
     public Receipt(String recipient) {
         this.recipient=recipient;
     }
     
+    public static void main(String[] args) {
+        sendReceipt("bsc218@lehigh.edu", "test receipt", "this is a test receipt");
+    }
+    
     public abstract void makeReceipt(Transaction t);
     
-    public void sendReceipt() {
+    public static void sendReceipt(String to, String subject, String body) {
         Properties props=System.getProperties();
         String host="smtp.gmail.com";
         props.put("mail.smtp.starttls.enable", "true");
@@ -38,18 +42,20 @@ public abstract class Receipt {
         Session session = Session.getDefaultInstance(props);
         MimeMessage message = new MimeMessage(session);
         try {
-            message.setSubject("Your scrumbags receipt");
-            message.setText("");
+            message.setFrom(new InternetAddress(userName));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            message.setSubject(subject);
+            message.setText(body);
             Transport transport = session.getTransport("smtp");
             transport.connect(host, userName, password);
             transport.sendMessage(message, message.getAllRecipients());
             transport.close();
         }
-        catch (AddressException ae) {
-            
+        catch (AddressException a) {
+            a.printStackTrace();
         }
         catch (MessagingException mex) {
-            
+            mex.printStackTrace();
         }
     }
 }
